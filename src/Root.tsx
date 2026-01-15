8a6500ab88da7722a02f488338f57abae71569b2
t3rmal <cristiangrimaldi305@gmail.com>
2026-01-15 13:36:39 +0000

    versione 2.0

--- src/Root.tsx
+++ src/Root.tsx
@@ -9,7 +9,6 @@ export const Root = () => {
 const Card = () => {
   return (
     <div className="flex flex-col bg-white border-2 border-black p-2">
-      
       {/* quadrato testo */}
       <div className="flex justify-center items-center border-2 border-black mb-2 h-12 font-bold">
         Pikachu - 0
